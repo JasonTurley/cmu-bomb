0fd2aa648529ac0226a5558852a94f2249033ce9
JasonTurley <jasonturley0@gmail.com>
2020-03-15 20:48:42 -0500

    add option to read input_file for answers

--- revbomb.c
+++ revbomb.c
@@ -9,6 +9,9 @@
 
 #define BUF_SIZE (128)
 
+/**
+ * Catches the Ctrl-C (SIGINT) signal.
+ */
 void sig_handler(int signo)
 {
     if (signo == SIGINT) {
@@ -26,12 +29,23 @@ void sig_handler(int signo)
     }
 }
 
-void read_line(char *buf)
+/**
+ * Read a line of text from the file, or from stdin if no file is given.
+ */
+void read_line(char *buf, FILE *file)
 {
-    fgets(buf, BUF_SIZE, stdin);
+    if (file)
+        fgets(buf, BUF_SIZE, file);
+    else
+        fgets(buf, BUF_SIZE, stdin);
+
     buf[strlen(buf) - 1] = '\0';
 }
 
+/**
+ * Detonates the bomb, effectively ending the game. Called when the player
+ * incorrectly answers a phase.
+ */
 void explode_bomb()
 {
     printf("\nBOOM!!!\n");
@@ -39,17 +53,32 @@ void explode_bomb()
     exit(8);
 }
 
+/**
+ * Wrapper for strcmp.
+ */
 int strings_not_equal(char *s, char *t)
 {
     return strcmp(s, t);
 }
 
+/**
+ * The first phase of the binary bomb game.
+ *
+ * Checks if the player enters the correct string passphrase.
+ */
 void phase_1(char *buf)
 {
     if (strings_not_equal(buf, "Public speaking is very easy."))
         explode_bomb();
 }
 
+/**
+ * Helper function for phase_2. Reads the first six numbers entered by the
+ * player and stores them in the `nums` return argument.
+ *
+ * If the player enters less than six numbers, the bomb explodes.
+ * If the player enters more than six numbers, the rest are ignored.
+ */
 void read_six_numbers(char *buf, int *nums)
 {
     int nums_read = sscanf(buf, "%d %d %d %d %d %d", &nums[0], &nums[1],
@@ -59,23 +88,27 @@ void read_six_numbers(char *buf, int *nums)
         explode_bomb();
 }
 
+/**
+ * The second phase of the binary bomb game.
+ *
+ */
 void phase_2(char *buf)
 {
     int nums[6];
+    int solution[] = {2, 6, 24, 120, 720};
 
-    // TODO: make valid C code
     read_six_numbers(buf, nums);
 
     if (nums[0] != 1)
         explode_bomb();
 
-    for (int i = 1; i <= 5; i++) {
-        nums[i] = (i + 1) * nums[i - 1];
-        // 1 2 6 24 120 720
-        // TODO: check each iteration of results
+    for (int i = 1; i < 6; i++) {
+        nums[i] = (i+1) * nums[i-1];
+
+        if (nums[i] != solution[i-1])
+            explode_bomb();
     }
 }
-
 /*
 int phase_3(char *buf)
 {
@@ -142,7 +175,23 @@ int main(int argc, char *argv[])
 {
     // TODO check arguments
     char buf[BUF_SIZE];
+    FILE *input_file = NULL;
 
+    if (argc > 2) {
+        printf("Usage: ./bomb [<input_file>]\n");
+        exit(8);
+    }
+
+    if (argc == 2) {
+        input_file = fopen(argv[1], "r");
+
+        if (!input_file) {
+            fprintf(stderr, "%s: Error: Couldn't open %s\n", argv[0], argv[1]);
+            exit(8);
+        }
+    }
+
+    // Setup signal handler
     if (signal(SIGINT, sig_handler) == SIG_ERR) {
         fprintf(stderr, "Failed to handle SIGINT\n");
         exit(1);
@@ -152,14 +201,15 @@ int main(int argc, char *argv[])
     printf("which to blow yourself up. Have a nice day!\n" );
 
     // Phase 1
-    read_line(buf);
+    read_line(buf, input_file);
     phase_1(buf);
     phase_defused();
     printf("Phase 1 defused. How about the next one?\n");
 
     // Phase 2
-    read_line(buf);
+    read_line(buf, input_file);
     phase_2(buf);
+    printf("That's number 2.  Keep going!\n");
 
     return 0;
 }
